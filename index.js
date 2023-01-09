d3995f40378b6666f6405a7055fcb20aed55b8bb
niiis <niiis>
2023-01-10 01:22:15 +0900

    初回コミット

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require(`fs`);
+const express = require("express");
+const app = express();
+app.use(express.urlencoded({ extended: true}));
+const activities = require("./activities.json");
+
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/autumn", function(req, res) {
+    console.log("reqの中身", req.body);
+    fs.writeFile(__dirname + "/data.txt", req.body.activity, function(){
+        res.send("投稿完了");
+    });
+});
+
+app.post("/update", function(req, res){
+    console.log(req.body.updateActivity)
+    activities[0].activity = req.body.updateActivity;
+    res.send(activities);
+});
+
+app.post("/delete", function(req, res){
+    console.log(req.body.number);
+    activities.splice(req.body.number, 1)
+    res.send(activities);
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, function() {
+    console.log(`Listening on localhost port ${port}`);
+});
